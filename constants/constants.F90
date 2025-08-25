16ed6b9825bd24c001a0343de5094beae1f12b55
Spencer Wong <spencer.wong@anu.edu.au>
2025-08-25 11:54:23 +1000

    Set hlv consistent with ESM1.6 UM and CICE

--- constants/constants.F90
+++ constants/constants.F90
@@ -48,7 +48,7 @@ real(kind=r8_kind), public, parameter :: GRAV_8 = 9.80665_r8_kind     !< Acceler
 real,               public, parameter :: RDGAS  = 287.05_r8_kind      !< Gas constant for dry air [J/kg/deg]
 real,               public, parameter :: RVGAS  = 461.50_r8_kind      !< Gas constant for water vapor [J/kg/deg]
 ! Extra:
-real,               public, parameter :: HLV      = 2.5e6_r8_kind     !< Latent heat of evaporation [J/kg]
+real,               public, parameter :: HLV      = 2.501e6_r8_kind   !< Latent heat of evaporation [J/kg]
 real,               public, parameter :: HLF      = 3.3358e5_r8_kind  !< Latent heat of fusion [J/kg]
 real,               public, parameter :: con_cliq = 4.1855e+3_r8_kind !< spec heat H2O liq [J/kg/K]
 real,               public, parameter :: con_csol = 2.1060e+3_r8_kind !< spec heat H2O ice [J/kg/K]
@@ -77,7 +77,7 @@ real,         public, parameter :: GRAV   = 9.80_r8_kind             !< Accelera
 real,         public, parameter :: RDGAS  = 287.04_r8_kind           !< Gas constant for dry air [J/kg/deg]
 real,         public, parameter :: RVGAS  = 461.50_r8_kind           !< Gas constant for water vapor [J/kg/deg]
 ! Extra:
-real,         public, parameter :: HLV = 2.500e6_r8_kind             !< Latent heat of evaporation [J/kg]
+real,         public, parameter :: HLV = 2.501e6_r8_kind             !< Latent heat of evaporation [J/kg]
 real,         public, parameter :: HLF = 3.34e5_r8_kind              !< Latent heat of fusion [J/kg]
 real,         public, parameter :: KAPPA  = 2.0_r8_kind/7.0_r8_kind  !< RDGAS / CP_AIR [dimensionless]
 real,         public, parameter :: CP_AIR = RDGAS/KAPPA              !< Specific heat capacity of dry air at constant pressure [J/kg/deg]
